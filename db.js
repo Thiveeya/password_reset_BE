0b7f281a5aace05ae1c0aa0403ff971766eb2bb0
Thiveeya <rbsthivi97@gmail.com>
2023-07-24 21:00:35 +0530

    ok

--- db.js
+++ db.js
@@ -1,14 +1,13 @@
-import { MongoClient } from "mongodb";
-import dotenv from "dotenv";
-dotenv.config();
+const {MongoClient} = require('mongodb');
+const client = new MongoClient("mongodb+srv://rbsthivi:thiveeya@cluster0.vgypaij.mongodb.net");
+   
 
-const MONGO_URL = process.env.MONGO_URL;
-
-async function dbConnect() {
-    const client = new MongoClient(MONGO_URL);
-    await client.connect();
-    console.log('DB connected')
-    return client;
-}
-
-export const client = await dbConnect();
+async function start(){
+    await client.connect()
+    console.log("Connected")
+    module.exports = client.db()
+    const app = require('./app')
+    app.listen(3000)
+  }
+  
+    start()
