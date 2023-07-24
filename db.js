de978bb8c9a436d11b464991f5adab2708173e47
Thiveeya <rbsthivi97@gmail.com>
2023-07-24 21:58:28 +0530

    sec

--- db.js
+++ db.js
@@ -1,11 +1,13 @@
 import { MongoClient } from "mongodb";
+import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
+app.use(express.json());
 
-const MONGO_URL = process.env.MONGO_URL;
+// const MONGO_URL = process.env.MONGO_URL;
 
 async function dbConnect() {
-    const client = new MongoClient(MONGO_URL,{ useNewUrlParser: true });
+    const client = new MongoClient("mongodb+srv://rbsthivi:thiveeya@cluster0.vgypaij.mongodb.net",{ useNewUrlParser: true });
     await client.connect();
     console.log('DB connected')
     return client;
