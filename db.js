b33455f79bf872c1185354406172791fd266ffb9
Thiveeya <rbsthivi97@gmail.com>
2023-07-24 21:15:06 +0530

    ok-2

--- db.js
+++ db.js
@@ -1,13 +1,27 @@
-const {MongoClient} = require('mongodb');
-const client = new MongoClient("mongodb+srv://rbsthivi:thiveeya@cluster0.vgypaij.mongodb.net");
+// const {MongoClient} = require('mongodb');
+// const client = new MongoClient("mongodb+srv://rbsthivi:thiveeya@cluster0.vgypaij.mongodb.net");
    
 
-async function start(){
-    await client.connect()
-    console.log("Connected")
-    module.exports = client.db()
-    const app = require('./app')
-    app.listen(3003)
-  }
+// async function start(){
+//     await client.connect()
+//     console.log("Connected")
+//     module.exports = client.db()
+//     const app = require('./app')
+//     app.listen(3003)
+//   }
   
-    start()
+//     start()
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+dotenv.config();
+
+const MONGO_URL = process.env.MONGO_URL;
+
+async function dbConnect() {
+    const client = new MongoClient(MONGO_URL);
+    await client.connect();
+    console.log('DB connected')
+    return client;
+}
+
+export const client = await dbConnect();
--- index.js
+++ index.js
@@ -1,36 +1,5 @@
-// const express=require('express');
-// require('dotenv').config()
-
-// const cors=require('cors');
-
-// const Routes=require('./Routes/user')
-// const Authentication=require('./Authentication/userAuth')
-// //port
-// const port = process.env.PORT
-
-// // initiating server
-// const app = express();
-
-// // middle ware
-// app.use(express.json());
-// app.use(cors());
-// app.use(Routes);
-// app.use(Authentication);
-// // application middleware
-// app.use("/user", userRouter);
-// app.use("/userall", getAllUser);
-// app.use("/home", isAuthenticated, authorizeLogin);
 
 
-// // intial landing page
-// app.get("/", (req, res) => {
-//     res.send("password reset API working good");
-// })
-
-// // listen and start http server in localhost
-// app.listen(port, () => {
-//     console.log(`Server connected in Localhost:${port}`);
-// })
 
 import express from "express";
 import dotenv from 'dotenv';
